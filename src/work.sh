8a37bd3712e2dffd4c876d6dcee5d53b9cdddb3f
Konstantin Vinogradov <exescript@gmail.com>
2020-06-22 22:59:04 +0300

    eXebash: minor fixes
    Changes:
    * fix mistypes
    * fix wrong debug test 'source' directive
    * other fixes

--- src/work.sh
+++ src/work.sh
@@ -7,38 +7,50 @@
 
 _cdir() { echo "$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"; }
 
-export work="$HOME/workspace"
+export _exe_work_workspace_default="$HOME/workspace2"
 
 export fenv="$work/env.sh"
 export futils="$work/utils.sh"
-
-function work_include() {
-	dbg "Workspace user directory: '$work'"
-
-	[ -d $work ] || {
-	    dbg "Workspace directory doesn't exist"
-	    dbg "Create workspace directory : '$work'"
-
-	    xquiet "mkdir $work" ||
-	        fatal "1" "make workspace failed: '$work'";
-	}
-
-	
-	for d in "${work_dirs[@]}"; do
-	    [ ! -d $work/$d ] && {
-	        dbg "Make workspace subdirectory: '$d' ";
-	        ( ! mkdir $work/$d ) && logf || logo;
-	    } || dbg "exedots: workspace subdirectory '$work/$d' already exists";
-	done
-}
-
-function work_exclude() {
-	dbg "work: exclude"
-}
-
 export docs="$work/docs"
 export scripts="$work/scripts"
 export src="$work/scripts/src"
 export build=$work/build
-	
 
+function workspace_init() {
+    local work="$1"
+    local work_dirs=( build logs tmp profiles bin lib scripts )
+
+    dbg "work: workspace dir: $work"
+
+    [ ! -d $work ] && {
+        xquiet "mkdir $work";
+        logstat "work: create workspace '$work/$d'";
+    }
+
+    for d in "${work_dirs[@]}"; do
+        if [ ! -d "$work/$d" ]; then
+            mkdir "$work/$d";
+            logstat "work: create $work/$d";
+        fi
+    done
+}
+
+function work_include() {
+    workspace_init "$_exe_work_workspace_default";
+}
+
+function work_exclude() {
+    dbg "work: exclude";
+}
+
+function work_init() {
+    dbg "work: init";
+}
+
+function work_run() {
+    dbg "work: run";
+}
+
+function work_help() {
+    dbg "work: help";
+}
